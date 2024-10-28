1961e21e46e461c46efb18615fc5c901d8fc4359
ethafa <31740787+ethafa@users.noreply.github.com>
2024-10-28 16:43:10 +0300

    Update sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -1,22 +1,20 @@
 let sketch2 = (p) => {
   p.setup = function() {
     p.createCanvas(400, 400);
-  p.background(250,250,100);
+    p.background(250, 250, 100); // Initial background color
   };
   
-    p.draw = function() {
-    
-    p.fill(250,200,200,50);
-  p.noStroke();
- p.ellipse(mouseX,mouseY,50,50);
-   
-      };
+  p.draw = function() {
+    p.fill(250, 200, 200, 50); // Semi-transparent fill color
+    p.noStroke();
+    p.ellipse(p.mouseX, p.mouseY, 50, 50); // Draw ellipse at mouse position
   };
+  
+  // Background color change on mouse press
   p.mousePressed = function() {
-    
-   p.background(230,0,0,40);
-   
-      };
+    p.background(230, 0, 0, 40); // Semi-transparent red background
   };
-  new p5(sketch2, 'sketch2'); // Attach sketch to the div with id="sketch2"
+};
 
+// Attach sketch to the div with id="sketch2"
+new p5(sketch2, 'sketch2');
