9db4c3284e7f16fc21fb0dc9bcd11377dda53af9
ethafa <31740787+ethafa@users.noreply.github.com>
2024-10-28 17:00:50 +0300

    Update sketch3.js

--- sketch3.js
+++ sketch3.js
@@ -1,21 +1,23 @@
 let sketch3 = (p) => {
   p.setup = function() {
     p.createCanvas(400, 400);
- 
   };
-  
+
   p.draw = function() {
-  p.background(0);
-  p.fill(0,255,0);
-  p.noStroke();
-  p.circle(mouseX,mouseY,50,50);
-  
-  p.fill(500,10,191);
-  p.circle(100,100,25,25);
-  p.circle(300,100,25,25);
-  p.circle(300,300,25,25);
-  p.circle(100,300,25,25);
-      };
+    p.background(0);
+
+    p.fill(0, 255, 0);
+    p.noStroke();
+    p.circle(p.mouseX, p.mouseY, 50); // Draw a circle following the mouse
+
+    // Draw stationary circles at the corners
+    p.fill(255, 10, 191);
+    p.circle(100, 100, 25);
+    p.circle(300, 100, 25);
+    p.circle(300, 300, 25);
+    p.circle(100, 300, 25);
   };
-  
-  new p5(sketch3, 'sketch3'); // Attach sketch to the div with id="sketch1"
+};
+
+// Attach the sketch to the div with id="sketch3"
+new p5(sketch3, 'sketch3');
