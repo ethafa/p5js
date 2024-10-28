abe951809dc23c8ffc41167e77e505e3f5170aa4
ethafa <31740787+ethafa@users.noreply.github.com>
2024-10-28 15:19:27 +0300

    Update sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -1,21 +1,23 @@
+// Sketch 2 in instance mode
+let sketch2 = (p) => {
+p.setup = function() {
+  p.createCanvas(400, 400);
+  p.background(250,250,100);
+};
 
-function setup() {
-  createCanvas(400, 400);
-  background(250,250,100);
-}
-
-function draw() {
+p.draw = function() {
   
-  fill(250,200,200,50);
-  noStroke();
-  ellipse(mouseX,mouseY,50,50);
+  p.fill(250,200,200,50);
+  p.noStroke();
+  p.ellipse(mouseX,mouseY,50,50);
  
+    };
+};
+
+p.mousePressed = function() {
+  p.background(230,0,0,40);
   
- 
 }
 
-function mousePressed() {
-  background(230,0,0,40);
-  
-}
+new p5(sketch2, 'sketch2'); // Attach sketch to the div with id="sketch2"
 
