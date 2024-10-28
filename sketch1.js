5b5c787812b69abc25baaf31ec2f4a3c298d5432
ethafa <31740787+ethafa@users.noreply.github.com>
2024-10-28 16:18:57 +0300

    Update sketch1.js

--- sketch1.js
+++ sketch1.js
@@ -1,30 +1,27 @@
-let sketch1 = (p) => {
-p.setup = function() {
-  p.createCanvas(400, 400);
-  p.background(250,250,100);
+function setup() {
+  createCanvas(400, 400);
+  background(250,250,100);
 };
 
-  p.draw = function() {
+  function draw() {
   
-  p.background(0);
+  background(0);
   
-  p.fill(0,255,0);
-  p.noStroke();
-  p.circle(200,200,50,50);
+  fill(0,255,0);
+  noStroke();
+  circle(200,200,50,50);
   
-  p.fill(140,0,191);
-  p.circle(100,100,25,25);
-  p.circle(300,100,25,25);
-  p.circle(300,300,25,25);
-  p.circle(100,300,25,25);
+  fill(140,0,191);
+  circle(100,100,25,25);
+circle(300,100,25,25);
+  circle(300,300,25,25);
+  circle(100,300,25,25);
   
-  p.stroke(255,255,255);
-  p.noFill();
-  p.rect(150,150,100,100);
-  p.rect(100,100,200,200);
+  stroke(255,255,255);
+  noFill();
+  rect(150,150,100,100);
+  rect(100,100,200,200);
  
     };
-};
 
-new p5(sketch1, 'sketch1'); // Attach sketch to the div with id="sketch1"
 
